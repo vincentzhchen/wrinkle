31e2629980d85655b4683ed102a0a2f944239787
Vincent Chen <vincent.zh.chen@gmail.com>
2018-12-22 23:24:31 -0500

    prototype

---
+++ wrinkle/__init__.py
@@ -0,0 +1,1 @@
+from wrinkle.core.wrinkle import Diff
---
+++ wrinkle/core/wrinkle.py
@@ -0,0 +1,148 @@
+import itertools
+import pandas as pd
+
+
+COMPARISON_COL = "COMPARISON_COL"
+
+
+class Diff(object):
+    def __init__(self, lhs, rhs, on, lhs_name="LHS", rhs_name="RHS",
+                 comparison_col_name="COMPARISON_COL", *args, **kwargs):
+        self.lhs = lhs
+        self.rhs = rhs
+        self.input_key = on
+        self.key = self.input_key + [comparison_col_name]
+        self.lhs_name = lhs_name
+        self.rhs_name = rhs_name
+        self.output_key = self.key + [self.lhs_name, self.rhs_name] + \
+                          ["DIFF", "ABS_DIFF", "PCT_DIFF", "ABS_PCT_DIFF"]
+
+        if self.lhs.index.name != self.input_key:
+            self.lhs.set_index(self.input_key, inplace=True)
+
+        if self.rhs.index.name != self.input_key:
+            self.rhs.set_index(self.input_key, inplace=True)
+
+        self._set_value_cols()
+
+        self.df = self.construct_diff_df()
+
+    def __str__(self):
+        return self.df.to_string()
+
+    def __repr__(self):
+        return self.df.to_string()
+
+    def _repr_html_(self):
+        # this is for displaying in IPython
+        return self.df._repr_html_()
+
+    def _set_value_cols(self):
+        self.value = list(set(self.lhs.columns).intersection(
+            self.rhs.columns))
+
+    def _construct_master_index_df(self):
+        """Helper function to construct unique index for the diff dataframe.
+        """
+        unique_idx = set(self.lhs.index.tolist() + self.rhs.index.tolist())
+        value_cols = list(set(self.lhs.columns).difference(self.input_key))
+        x_prod_idx = tuple(list(k[0]) + [k[1]] for k in
+                           list(itertools.product(unique_idx, value_cols)))
+        df = pd.MultiIndex.from_tuples(
+            x_prod_idx, names=self.key).to_frame()
+        df = df.sort_index()  # speed up lex sort
+        df = df.reset_index(drop=True)
+        return df
+
+    def _separate_duplicates(self, df, key, series_name):
+        df = df.stack().reset_index()
+        df.columns = key + [series_name]
+        return df.drop_duplicates(), df[df.duplicated()]
+
+    def _split_out_numeric_values(self, df):
+        df["IS_LHS_NUMERIC"] = pd.to_numeric(df[self.lhs_name], errors="coerce")
+        df["IS_RHS_NUMERIC"] = pd.to_numeric(df[self.rhs_name], errors="coerce")
+
+        numeric = df["IS_LHS_NUMERIC"].notnull() | df[
+            "IS_RHS_NUMERIC"].notnull()
+        index_cols = self.key + ["IS_DUPLICATED"]
+        value_cols = [self.lhs_name, self.rhs_name]
+
+        df_numeric = df[numeric][index_cols + value_cols]
+        for c in value_cols:
+            df_numeric[c] = pd.to_numeric(df_numeric[c])
+
+        df_non_numeric = df[~numeric][index_cols + value_cols]
+
+        return df_numeric, df_non_numeric
+
+    def _diff_numeric(self, df):
+        """Calculate differences for numeric values
+
+        Do not fill missing values; allow the differences to have nulls.
+
+        """
+        not_null = df[self.lhs_name].notnull() & df[self.rhs_name].notnull()
+        df.loc[not_null, "DIFF"] = df.loc[not_null, self.lhs_name] - \
+                                   df.loc[not_null, self.rhs_name]
+        df.loc[not_null, "ABS_DIFF"] = df.loc[not_null, "DIFF"].abs()
+
+        # PCT_DIFF = RHS / LHS - 1
+        df.loc[not_null, "PCT_DIFF"] = df.loc[not_null, self.rhs_name] / \
+                                       df.loc[not_null, self.lhs_name] - 1
+        df.loc[not_null, "ABS_PCT_DIFF"] = df.loc[not_null, "PCT_DIFF"].abs()
+
+        return df
+
+    def _diff_non_numeric(self, df):
+        """Display differences for non-numeric values.
+
+        TODO: maybe calculate string difference here
+
+        """
+        df["DIFF"] = df["ABS_DIFF"] = df["PCT_DIFF"] = df["ABS_PCT_DIFF"] = \
+            df[self.lhs_name] == df[self.rhs_name]
+        df = df.loc[~df["DIFF"]]
+        result = df.loc[:, self.lhs_name] + " <> " + df.loc[:, self.rhs_name]
+        df.loc[:, "DIFF"] = result
+        df.loc[:, "ABS_DIFF"] = result
+        df.loc[:, "PCT_DIFF"] = result
+        df.loc[:, "ABS_PCT_DIFF"] = result
+        return df
+
+    def _generate_diffs(self, df):
+        numeric, non_numeric = self._split_out_numeric_values(df)
+
+        numeric = self._diff_numeric(df=numeric)
+        numeric = numeric.sort_values(["ABS_PCT_DIFF"], ascending=False)
+        non_numeric = self._diff_non_numeric(df=non_numeric)
+
+        df = pd.concat([numeric, non_numeric], ignore_index=True)
+        return df
+
+    def construct_diff_df(self):
+        master_index_df = self._construct_master_index_df()
+        lhs, lhs_duplicates = self._separate_duplicates(
+            df=self.lhs, key=self.key, series_name="LHS")
+        rhs, rhs_duplicates = self._separate_duplicates(
+            df=self.rhs, key=self.key, series_name="RHS")
+
+        # left join twice is more efficient than an outer join
+        df = pd.merge(left=master_index_df, right=lhs, how="left", on=self.key)
+        df = pd.merge(left=df, right=rhs, how="left", on=self.key)
+
+        # flag duplicates
+        df["IS_DUPLICATED"] = 0
+        lhs_duplicates["IS_DUPLICATED"] = 1
+        rhs_duplicates["IS_DUPLICATED"] = 1
+        df = pd.concat([df, lhs_duplicates, rhs_duplicates],
+                       ignore_index=True, sort=True)
+
+        df = self._generate_diffs(df)
+        for c in ["DIFF", "ABS_DIFF", "PCT_DIFF", "ABS_PCT_DIFF"]:
+            df.loc[df["IS_DUPLICATED"] & df[
+                self.lhs_name].notnull(), c] = f"DUPLICATED_ON_{self.lhs_name}"
+            df.loc[df["IS_DUPLICATED"] & df[
+                self.rhs_name].notnull(), c] = f"DUPLICATED_ON_{self.rhs_name}"
+
+        return df[self.output_key]
